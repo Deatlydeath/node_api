408a95ee4be1956a89769493814bcd61ceefb9a9
Bas van der Horst <bas4iphone4@gmail.com>
2019-01-11 10:52:17 +0100

    poort update
    poort update zodat deze door heroku gebruikt kan worden.

--- notable/server.js
+++ notable/server.js
@@ -5,7 +5,7 @@ const bodyParser     = require('body-parser');
 const db             = require('./config/db');
 const mongoose       = require('mongoose');
 const app            = express();
-const port = 8000;
+const port = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 MongoClient.connect(db.url, (err, database) => {
   if (err) return console.log(err)
